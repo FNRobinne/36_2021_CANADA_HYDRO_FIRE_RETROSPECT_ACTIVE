c2fca55417517c340f8c12d34af59d27c48a6478
merlinlelutin <francois.robinne@nrcan-rncan.gc.ca>
2021-09-07 15:49:10 -0600

    Start working with HYDAT

--- Retrieve_HYDAT_Stations.R
+++ Retrieve_HYDAT_Stations.R
@@ -1,45 +1,46 @@
 ## ---------------------------
 ##
-## Script name: 
+## Script name: Retrieve_HYDAT_Stations
 ##
-## Purpose of script:
+## Purpose of script: Search and fetch HYDAT stations ID and location
 ##
-## Author: Dr. Timothy Farewell
+## Author: Dr. François-Nicolas Robinne
 ##
-## Date Created: 2018-04-11
+## Date Created: 07-09-2021
 ##
-## Copyright (c) Timothy Farewell, 2018
-## Email: hello@timfarewell.co.uk
+## Copyright (c) François-Nicolas Robinne, 2021
+## Email: francois.robinne@nrcan-rncan.gc.ca
 ##
 ## ---------------------------
 ##
-## Notes:
+## Notes: This script is part of an NRCan project focusing on western Canadian water supply
+##        It can be reused to extend the scope of work to the entire country
 ##   
 ##
 ## ---------------------------
 
 ## set working directory for Mac and PC
 
-setwd("~/Google Drive/")      # Tim's working directory (mac)
-setwd("C:/Users/tim/Google Drive/")    # Tim's working directory (PC)
+setwd("D:/PROJECTS/36_2021_CANADA_HYDRO_FIRE_RETROSPECT_ACTIVE")
 
-## ---------------------------
+# Options -----------------------------------------------------------------
 
 options(scipen = 6, digits = 4) # I prefer to view outputs in non-scientific notation
 memory.limit(30000000)     # this is needed on some PCs to increase memory allowance, but has no impact on macs.
 
-## ---------------------------
+# Load packages -----------------------------------------------------------
 
-## load up the packages we will need:  (uncomment as required)
+library(tidyverse)
+library(tidyhydat)
 
-require(tidyverse)
-require(data.table)
-# source("functions/packages.R")       # loads up all the packages we need
+# Retrieve HYDAT stations -------------------------------------------------
 
-## ---------------------------
+# Get stations for western Canadian provinces and territories
+# Namely: British Columbia (BC), Alberta (AB), Yukon (YK), and Northwest Territories (NT)
+west_stns <- hy_stations() %>%
+  filter(PROV_TERR_STATE_LOC == "BC" | PROV_TERR_STATE_LOC == "BC" | PROV_TERR_STATE_LOC == "YK" | PROV_TERR_STATE_LOC == "NT" ) %>%
+  pull_station_number()
 
-## load up our functions into memory
 
-# source("functions/summarise_data.R") 
-
-## ---------------------------
+# Disconnect from HYDAT
+hy_src_disconnect()
